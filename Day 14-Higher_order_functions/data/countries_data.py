6c2f638d6f3a8a6790620cbd056373c31cc5af79
harikadungala-gitaa <harikadungala@gitaa.in>
2025-11-12 10:33:49 +0530

    Add files via upload

---
+++ Day 14-Higher_order_functions/data/countries_data.py
@@ -0,0 +1,160 @@
+# data/countries_data.py
+
+countries_data = [
+    {
+        "name": "Afghanistan",
+        "capital": "Kabul",
+        "languages": ["Pashto", "Uzbek", "Turkmen"],
+        "population": 27657145
+    },
+    {
+        "name": "Albania",
+        "capital": "Tirana",
+        "languages": ["Albanian"],
+        "population": 2886026
+    },
+    {
+        "name": "Algeria",
+        "capital": "Algiers",
+        "languages": ["Arabic"],
+        "population": 40400000
+    },
+    {
+        "name": "Andorra",
+        "capital": "Andorra la Vella",
+        "languages": ["Catalan"],
+        "population": 78014
+    },
+    {
+        "name": "Angola",
+        "capital": "Luanda",
+        "languages": ["Portuguese"],
+        "population": 25868000
+    },
+    {
+        "name": "Argentina",
+        "capital": "Buenos Aires",
+        "languages": ["Spanish", "Guaraní"],
+        "population": 43590400
+    },
+    {
+        "name": "Armenia",
+        "capital": "Yerevan",
+        "languages": ["Armenian"],
+        "population": 2994400
+    },
+    {
+        "name": "Australia",
+        "capital": "Canberra",
+        "languages": ["English"],
+        "population": 24117360
+    },
+    {
+        "name": "Austria",
+        "capital": "Vienna",
+        "languages": ["German"],
+        "population": 8725931
+    },
+    {
+        "name": "Azerbaijan",
+        "capital": "Baku",
+        "languages": ["Azerbaijani"],
+        "population": 9730500
+    },
+    {
+        "name": "Bangladesh",
+        "capital": "Dhaka",
+        "languages": ["Bengali"],
+        "population": 161006790
+    },
+    {
+        "name": "Brazil",
+        "capital": "Brasília",
+        "languages": ["Portuguese"],
+        "population": 206135893
+    },
+    {
+        "name": "Canada",
+        "capital": "Ottawa",
+        "languages": ["English", "French"],
+        "population": 36155487
+    },
+    {
+        "name": "China",
+        "capital": "Beijing",
+        "languages": ["Chinese"],
+        "population": 1377422166
+    },
+    {
+        "name": "Denmark",
+        "capital": "Copenhagen",
+        "languages": ["Danish"],
+        "population": 5717014
+    },
+    {
+        "name": "Finland",
+        "capital": "Helsinki",
+        "languages": ["Finnish", "Swedish"],
+        "population": 5491817
+    },
+    {
+        "name": "France",
+        "capital": "Paris",
+        "languages": ["French"],
+        "population": 66710000
+    },
+    {
+        "name": "Germany",
+        "capital": "Berlin",
+        "languages": ["German"],
+        "population": 81770900
+    },
+    {
+        "name": "India",
+        "capital": "New Delhi",
+        "languages": ["Hindi", "English"],
+        "population": 1295210000
+    },
+    {
+        "name": "Japan",
+        "capital": "Tokyo",
+        "languages": ["Japanese"],
+        "population": 126960000
+    },
+    {
+        "name": "Nigeria",
+        "capital": "Abuja",
+        "languages": ["English"],
+        "population": 186988000
+    },
+    {
+        "name": "Norway",
+        "capital": "Oslo",
+        "languages": ["Norwegian"],
+        "population": 5232929
+    },
+    {
+        "name": "Russia",
+        "capital": "Moscow",
+        "languages": ["Russian"],
+        "population": 146599183
+    },
+    {
+        "name": "Sweden",
+        "capital": "Stockholm",
+        "languages": ["Swedish"],
+        "population": 9894888
+    },
+    {
+        "name": "United Kingdom",
+        "capital": "London",
+        "languages": ["English"],
+        "population": 65110000
+    },
+    {
+        "name": "United States",
+        "capital": "Washington, D.C.",
+        "languages": ["English"],
+        "population": 323947000
+    }
+]
